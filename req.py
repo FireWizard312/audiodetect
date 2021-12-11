dabb35dd734090160638863ac9fbdb7fe322247d
Matthew Liu <matthew.liu312@gmail.com>
2021-12-11 00:27:45 -0500

    Added honk

--- req.py
+++ req.py
@@ -7,10 +7,10 @@ serial = spi(port=0, device=0, gpio=noop())
 device = max7219(serial)
 
 siren = [(0,7),(1,7),(2,7),(3,7),(4,7),(5,7),(6,7),(7,7),(2,6),(3,6),(4,6),(5,6),(2,5),(3,5),(4,5),(5,5),(2,4),(3,4),(4,4),(5,4),(3,3),(4,3),(1,0),(2,1),(5,1),(6,0)]
-
+honk = [(1,3),(2,3),(1,4),(2,4),(3,3),(4,3),(5,3),(3,4),(4,4),(5,5),(5,2),(5,5)]
 for i in range(3):
     with canvas(device) as draw:
-        draw.point(siren, fill = "white")
+        draw.point(honk, fill = "white")
         # draw.point((2,6), fill = "white")
         # draw.point((3,6), fill = "white")
         # draw.point((4,6), fill = "white")
@@ -39,7 +39,7 @@ for i in range(3):
 
     time.sleep(2)
     with canvas(device) as draw:
-        draw.point(siren, fill = "black")
+        draw.point(honk, fill = "black")
     #     draw.point((2,6), fill = "black")
     #     draw.point((3,6), fill = "black")
     #     draw.point((4,6), fill = "black")
