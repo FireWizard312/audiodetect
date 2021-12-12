6c086fe5d108fd579a10d1c21ee365b666200691
Matthew Liu <matthew.liu312@gmail.com>
2021-12-11 19:54:41 -0500

    Trim code add lights to web

--- web.py
+++ web.py
@@ -2,10 +2,18 @@
 
 # Let's get this party started!
 from wsgiref.simple_server import make_server
-
+from luma.led_matrix.device import max7219
+from luma.core.interface.serial import spi, noop
+from luma.core.render import canvas
+import time
 import falcon
 
 
+serial = spi(port=0, device=0, gpio=noop())
+device = max7219(serial)
+
+siren = [(0,7),(1,7),(2,7),(3,7),(4,7),(5,7),(6,7),(7,7),(2,6),(3,6),(4,6),(5,6),(2,5),(3,5),(4,5),(5,5),(2,4),(3,4),(4,4),(5,4),(3,3),(4,3),(1,0),(2,1),(5,1),(6,0)]
+honk = [(0,3),(1,3),(0,4),(1,4),(0,2),(1,2),(0,5),(1,5),(2,3),(3,3),(4,3),(2,4),(3,4),(4,4),(4,2),(4,5),(6,2),(7,1),(6,5),(7,6)]
 # Falcon follows the REST architectural style, meaning (among
 # other things) that you think in terms of resources and state
 # transitions, which map to HTTP verbs.
@@ -14,8 +22,20 @@ class ThingsResource:
         class_id = req.get_param('class_id')
         resp.status = falcon.HTTP_200  # This is the default status
         resp.content_type = falcon.MEDIA_TEXT  # Default is JSON, so override
-        resp.text = (f'I see you sent me {class_id}')
-        print(f'hi {class_id}')
+        class_id = int(class_id)
+        if class_id == 8:
+            while True:
+                with canvas(device) as draw:
+                    draw.point(siren, fill = "white")
+                time.sleep(0.75)
+                with canvas(device) as draw:
+                    draw.point(siren, fill = "black")
+                time.sleep(0.5)
+        else:
+            with canvas(device) as draw:
+                draw.point(siren, fill = "black")
+                draw.point(honk, fill = "black")
+                    
 
 # falcon.App instances are callable WSGI apps
 # in larger applications the app is created in a separate file
