91f760ea1e9895c21dfbfba1ce1684b374ad5b90
Matthew Liu <matthew.liu312@gmail.com>
2021-12-12 13:13:11 -0500

    Cleaned up web

--- web.py
+++ web.py
@@ -12,10 +12,8 @@ class ThingsResource:
         resp.content_type = falcon.MEDIA_TEXT  # Default is JSON, so override
         class_id = int(class_id)
         if class_id == 8:
-            light_tasks.lightsoff.delay()
             light_tasks.sirenlightson.delay()
         elif class_id == 1:
-            light_tasks.lightsoff.delay()
             light_tasks.honklightson.delay()
         else:
             light_tasks.lightsoff.delay()
